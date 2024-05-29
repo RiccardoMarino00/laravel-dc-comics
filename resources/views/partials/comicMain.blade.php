a00cccdc48324771258415f1b934e32b5cf8bfaf
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-29 16:21:32 +0200

    Added: model, controller, migration and in web.php added route index

---
+++ resources/views/partials/comicMain.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <p>Comic Main</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '<h1>layout</h1>';
 });
+
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
